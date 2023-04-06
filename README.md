205f47f69a0f0aeb7a60880fc62e7728e4b0ef64
Victor Yevitchenko <cheer4engineer@gmail.com>
2023-04-06 19:28:23 +0500

    First commit mostly about calls.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# gsm-ip-gateway
+# GSM IP Gateway
+
+## OpenWRT
+
+```shell
+scp -O * root@IP:/tmp  
+ssh root@IP
+```  
+Let's say we have IP=192.168.1.100
+Then:
+```shell
+scp -O * root@192.168.1.100:/tmp`  
+ssh root@192.168.1.100`
+```
+In ssh console:
+```
+cd /tmp  
+./install.sh
+```
+
+Some hints:  
+`pjsip show endpoints`  
+`core show translation` - show transcode matrix  
+`core show channel` - show active channel details (codec etc)  
+`core show translation paths gsm`
---
+++ install.sh
@@ -0,0 +1,38 @@
+install_dependencies_for_openwrt() {
+  opkg update
+  opkg install asterisk asterisk-pjsip asterisk-bridge-simple asterisk-codec-alaw asterisk-res-rtp-asterisk
+  opkg install kmod-usb-serial kmod-usb-serial-wwan kmod-usb-serial-option usb-modeswitch asterisk-chan-dongle
+  opkg install asterisk-func-base64 asterisk-app-system
+}
+
+add_line() {
+  if ! grep -q -F "$1" "$2"; then
+    echo "$1" >>"$2"
+  fi
+}
+
+install_configs() {
+  add_line "#include pjsip_custom.conf" /etc/asterisk/pjsip.conf
+  add_line "#include extensions_custom.conf" /etc/asterisk/extensions.conf
+  add_line "noload => chan_sip.so" /etc/asterisk/modules.conf
+  add_line "load => chan_pjsip.so" /etc/asterisk/modules.conf
+  cp pjsip_custom.conf pjsip_custom_100.conf pjsip_custom_101.conf extensions_custom.conf dongle.conf /etc/asterisk/
+}
+
+install_for_openwrt() {
+  echo "Install Asterisk for OpenWRT"
+  install_dependencies_for_openwrt
+  chmod 666 /dev/ttyUSB*
+  install_configs
+  service asterisk restart
+}
+
+install_for_deb() {
+  echo "install for deb based OS"
+}
+
+if [ "$(uname -n)" = "OpenWrt" ]; then
+  install_for_openwrt
+else
+  install_for_deb
+fi
