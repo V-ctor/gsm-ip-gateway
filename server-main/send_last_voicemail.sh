fbf5380c94b4cb112a32d89095b2165b1beba7f6
Victor Yevitchenko <cheer4engineer@gmail.com>
2023-06-04 00:24:55 +0500

    Now all voice-mail records are sent by email.

--- server-main/send_last_voicemail.sh
+++ server-main/send_last_voicemail.sh
@@ -3,6 +3,7 @@
 tokens_file=/etc/asterisk/extensions_tokens.conf
 BOT_ID=$(grep -Po '(?<=BOT_ID=).*' "$tokens_file")
 CHAT_ID=$(grep -Po '(?<=CHAT_ID=).*' "$tokens_file")
+EMAIL_TO=$(grep -Po '(?<=EMAIL_TO=).*' "$tokens_file")
 
 voicemail_dir="/var/spool/asterisk/voicemail/voice-mail/common/INBOX"  # Replace with the desired voicemail directory and mailbox
 
@@ -17,6 +18,7 @@ echo "$wav_file"
 echo "$txt_file"
 opus_file="${wav_file%.wav}.opus"
 opusenc "$wav_file" "$opus_file"
+#description="Call from $caller_num at $date_time"
 
 result=$(curl -X POST -H "Content-Type:multipart/form-data"\
   -F document=@"$opus_file" "https://api.telegram.org/$BOT_ID/sendDocument?chat_id=$CHAT_ID"\
@@ -24,7 +26,10 @@ result=$(curl -X POST -H "Content-Type:multipart/form-data"\
 
 tg_status=$(echo "$result" | grep -oP '"ok"\s*:\s*\K[^,}]+')
 
-if [ "$tg_status" = "true" ]; then
+email_result=$(cat "$txt_file" | /usr/bin/mutt -F /etc/Muttrc -s "Voice record"  "$EMAIL_TO" -a "$opus_file")
+echo "$email_result"
+
+if [[ "$tg_status" = "true" || $? -eq 0 ]]; then
     all_last_records_files="${wav_file%.wav}.*"
     echo $all_last_records_files | xargs rm -v  #it's important to avoid quotes for $all_last_records_files in spite of warning
 fi
