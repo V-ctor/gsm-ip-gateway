241d271ea4e6b872efa37dcb86033619c580dddf
Victor Yevitchenko <cheer4engineer@gmail.com>
2023-10-31 12:28:08 +0500

    It brings fix for E171 from MTS (setup dongle by IMEI).

--- install_asterisk_client.sh
+++ install_asterisk_client.sh
@@ -35,6 +35,27 @@ install_configs() {
   done
 }
 
+# The issue, if we have:
+# 1. dongle E171 from MTS (AE171s-1), lsusb recognizes as
+#  12d1:1506        Mobile Connect
+# 2. and 'asterisk-chan-dongle - 2021-10-06-3d046f7d-2'
+# then Asterisk can't find dongle if it is described in dongle.conf not by audio/data ports (ttyUSBx) but IMEI.
+# Also Asterisk can't discover such dongle by 'dongle discovery' command. It was solved in commit https://github.com/wdoekes/asterisk-chan-dongle/commit/503dba87d726854b74b49e70679e64e6e86d5812
+# but 'asterisk-chan-dongle - 2021-10-06-3d046f7d-2' does not contain it. That is why we emulate it by this modifying.
+modify_chan_dongle_for_E171() {
+  file_to_modify="/usr/lib/asterisk/modules/chan_dongle.so"
+  current_sha256=$(sha256sum "$file_to_modify" | awk '{print $1}')
+
+  # Check if the current hash matches the expected hash
+  if [ "$current_sha256" = "6626caa88d2c5b0799376961dc6809f1f4af510e67818dc740e56587d3d8a25e" ]; then
+      # Writing the value to the specific position in the file (198872 bytes from the beginning)
+      printf '\x01' | dd of="$file_to_modify" bs=1 seek=198872 conv=notrunc
+      echo "Value '0x01' written to $file_to_modify at position 198872."
+  else
+      echo "File $file_to_modify does not match the expected SHA256 hash. Looks it is not '2021-10-06-3d046f7d-2' version. Not modifying the file."
+  fi
+}
+
 install_for_openwrt() {
   echo "Install Asterisk for OpenWRT"
   install_dependencies_for_openwrt
@@ -43,6 +64,7 @@ install_for_openwrt() {
   /etc/init.d/hotplug2 restart
 
   install_configs
+  modify_chan_dongle_for_E171
   service asterisk restart
 }
 
